4e7f574d1871c9c82ba851d39fd1af86e195be93
maksimtokyo <maksimtokyooo@yandex.ru>
2025-05-04 23:38:31 +0300

    second update

--- main.cpp
+++ main.cpp
@@ -5,17 +5,5 @@
 
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    PdfParser m_pars;
-    m_pars.readfiles({
-        "test1.pdf",
-        // "test2.pdf",
-        // "test3.pdf",
-        // "test4.pdf",
-    });
-    m_pars.decompress_refs();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
 
 }
